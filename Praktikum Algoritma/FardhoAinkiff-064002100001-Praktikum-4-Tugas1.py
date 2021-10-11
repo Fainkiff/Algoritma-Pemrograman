37fd9428a76cba04955c1b7e70caa71277734361
Fainkiff <91054499+Fainkiff@users.noreply.github.com>
2021-10-11 13:47:07 +0700

    Commit Praktikum 4 Tugas 1

---
+++ Praktikum Algoritma/FardhoAinkiff-064002100001-Praktikum-4-Tugas1.py
@@ -0,0 +1,12 @@
+@author: acer
+Nama : Fardho Ainkiff Rasya
+NIM  : 064002100001
+"""
+
+x = int(input("masukan angka : "))
+
+while x > 0 :
+    for b in range(x):
+        print(x, end='')
+    print()
+    x -=1
