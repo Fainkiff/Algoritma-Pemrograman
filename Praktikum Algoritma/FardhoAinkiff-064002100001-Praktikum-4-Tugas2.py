f6499abad7e546212a17f9fa01d864c0f2ee0e07
Fainkiff <91054499+Fainkiff@users.noreply.github.com>
2021-10-17 22:31:41 +0700

    Commit Praktikum 4 Tugas 2

--- Praktikum Algoritma/FardhoAinkiff-064002100001-Praktikum-4-Tugas2.py
+++ Praktikum Algoritma/FardhoAinkiff-064002100001-Praktikum-4-Tugas2.py
@@ -4,19 +4,25 @@ NIM  : 064002100001
 """
 
 print("Program ini akan menentukan banyak hari pada bulan dan tahun yang diminta")
-print("Ketik n untuk berhenti")
-
-
-ulang = "Y"
-while ulang == "Y" or ulang == "y" :
-    bulan = int(input("Masukan Bulan : "))
-    tahun = int(input("Masukan Tahun : "))
-    if(bulan == 1 or bulan == 3 or bulan == 5 or bulan == 7 or bulan == 8 or bulan == 10 or bulan == 12):
-        print("Ada 31 hari")
-    elif((bulan == 2) and ((tahun%400==0) or (tahun%4==0 and tahun%100!=0))):
-        print("Ada 29 hari")
-    elif(bulan == 2):
-        print("Ada 28 hari")
+ulang = True
+while ulang == True:
+    print("Masukan 0 untuk menghentikan program")
+    bulan = int(input("Masukan Bulan: "))
+    if bulan == 0:
+        ulang = False
+        print("")
     else:
-        print("Ada 30 hari")
-    ulang = input("coba lagi? ")
+        if bulan == 1 or bulan == 2 or bulan == 3 or bulan == 4 or bulan == 5 or bulan == 6 or bulan == 7 or bulan == 8 or bulan == 9 or bulan == 10 or bulan == 11 or bulan == 12:
+            if bulan == 2:
+                tahun = int(input("Masukan Tahun (cont. 2021): "))
+                if (tahun%4==0) or (tahun%400==0):
+                    print("Ada 29 Hari")
+                else:
+                    print("Ada 28 Hari")
+            else:
+                if(bulan == 1 or bulan == 3 or bulan == 5 or bulan == 7 or bulan == 8 or bulan == 10 or bulan == 12):
+                    print("Ada 31 Hari")
+                else:
+                    print("Ada 30 Hari")
+        else:
+            print("Angka yang dimasukan tidak valid", bulan)
